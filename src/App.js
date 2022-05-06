90453b916033d071cdc188a3d7b3219d86061547
Nargis21 <nargisakther1933@gmail.com>
2022-05-06 11:38:43 +0600

    firebase initialize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is client side!</h1>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { Carousel } from 'bootstrap';
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h1>
+                This is banner
+            </h1>
+        </div>
+    );
+};
+
+export default Banner;
